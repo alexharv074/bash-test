aa533a7ad5cab9dee683798f72dbaedfd604053c
Alex Harvey <alexharv074@gmail.com>
2019-02-13 19:04:32 +1100

    Forgotten

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
-while getopts "ab:" opt; do
-  case $opt in
-  a)
-    foo=true
-    ;;
-  b)
-    bar=$OPTARG
-    ;;
-  esac
-done
-shift $((OPTIND -1))
-echo "You passed $bar to -b, -a is $foo, and then passed $* after that"
+name=test123
+instance=oracle
+schedname=$name"_"$instance
+fullname=$schedname"_EXP"
+echo -e $fullname
