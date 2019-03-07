305a3d02afcd0dbfd71d5022b1836868b56b261a
Alex Harvey <alexharv074@gmail.com>
2019-03-08 09:53:14 +1100

    Second go

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ declare -a arr=(node_modules/* package-lock.json bundle.js.map)
 function deploy(){
     echo aws s3 rm s3://bucketname --profile Administrator --recursive
     echo aws s3 sync ./ s3://bucketname --profile Administrator \
-      $(for x in "${arr[@]}" ; do echo "--exclude=$x " ; done)
+      "${arr[@]/#/--exclude=}"
 }
 
 deploy
