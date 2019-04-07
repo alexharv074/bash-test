cc1200940c4a101a1a3093e5e49c0182abb54f42
Alex Harvey <alexharv074@gmail.com>
2019-04-07 17:18:34 +1000

    https://stackoverflow.com/q/33947274/3787051
    Sort alphabetically lines between 2 patterns in Bash.

--- test.sh
+++ test.sh
@@ -1,54 +1,76 @@
-setUp() {
-  cat > /tmp/before <<EOF
-aaa
-bbb
-ccc
-bbb
-ccc
-eee
-EOF
-  cat > /tmp/expected <<EOF
-aaa
-bbb
-ccc
-bbb
-ccc
-ddd
-eee
-EOF
-}
+#!/usr/bin/env bash
 
-script_under_test1() {
-  cat <<EOF
-function! AddLine()
-  call search("bbb")
-  call search("bbb")
-  let l:foundline = search("ccc")
-  call append(l:foundline, "ddd")
-  wq!
-endfunction
-EOF
-}
+data="aaa
+bbb
+PATTERN1
+foo
+bar
+baz
+qux
+PATTERN2
+ccc
+ddd"
 
-script_under_test2() {
-  cat <<EOF
-function! AddLine()
-  normal /bbbn/cccoddd
-  wq!
-endfunction
-EOF
-}
+expected="aaa
+bbb
+PATTERN1
+bar
+baz
+foo
+qux
+PATTERN2
+ccc
+ddd"
 
-testIt1() {
-  script_under_test1 > /tmp/script.vim
-  vim -u /tmp/script.vim -c 'call AddLine()' /tmp/before
-  assertEquals "" "$(diff -u /tmp/expected /tmp/before)"
+testIt1() { response=$(gawk '/^PATTERN1/ {f=1;delete a}
+     /^PATTERN2/ {f=0; n=asort(a); for (i=1;i<=n;i++) print a[i]}
+     !f
+     f{a[$0]=$0}' <<< "$data")
+  assertEquals "$expected" "$response"
 }
 
 testIt2() {
-  script_under_test2 > /tmp/script.vim
-  vim -u /tmp/script.vim -c 'call AddLine()' /tmp/before
-  assertEquals "" "$(diff -u /tmp/expected /tmp/before)"
+  response=$(gawk '/^PATTERN1/ {f=1} /^PATTERN2/ {f=0; n=asort(a); for (i=1;i<=n;i++) print a[i]} !f; f{a[$0]=$0}' <<< "$data")
+  assertEquals "$expected" "$response"
+}
+
+testIt3() {
+  response=$(gawk -v p=1 '
+    /^PATTERN2/ {          # when we we see the 2nd marker:
+
+        # close the "write" end of the pipe to sort. Then sort will know it
+        # has all the data and it can begin sorting
+        close("sort", "to");
+
+        # then sort will print out the sorted results, so read and print that
+        while (("sort" |& getline line) >0) print line 
+
+        # and turn the boolean back to true
+        p=1
+    }
+    p  {print}             # if p is true, print the line
+    !p {print |& "sort"}   # if p is false, send the line to `sort`
+    /^PATTERN1/ {p=0}      # when we see the first marker, turn off printing
+    ' <<< "$data")
+  assertEquals "$expected" "$response"
+}
+
+testIt4() {
+  response=$(
+    sed '1,/^PATTERN1$/!d' <<< "$data"
+    sed '/^PATTERN1$/,/^PATTERN2$/!d' <<< "$data" | head -1 | tail -n +2 | sort
+    sed '/^PATTERN2$/,$!d' <<< "$data"
+  )
+  assertEquals "$expected" "$response"
+}
+
+testIt5() {
+  response=$(
+    gsed -n '1,/PATTERN1/p' <<< "$data"
+    gsed   '1,/PATTERN1/d;/PATTERN2/Q' <<< "$data" | sort
+    gsed -n '/PATTERN2/,$p' <<< "$data"
+  )
+  assertEquals "$expected" "$response"
 }
 
 . shunit2
