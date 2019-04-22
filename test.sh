a35ce59541561f51b795243361c6448f2cc3c241
Alex Harvey <alexharv074@gmail.com>
2019-04-22 14:43:21 +1000

    GNU regexp enabled find command question

--- test.sh
+++ test.sh
@@ -1,16 +1,5 @@
-data="xx
-xx
-PATTERN1
-c
-a
-PATTERN2
-yy
-yy"
-
-gsed -n '
-  0,/PATTERN1/p
-  /PATTERN1/,/PATTERN2/H
-  /PATTERN2/ {
-    g;s/\nPATTERN1\n\(.*\)\n\(PATTERN2.*\)/echo "\1" | sort;echo "\2"/e
-  }
-  /PATTERN2/,$p' <<< $data
+mkdir node_fake_returns
+mkdir output_json
+for i in 1 2 3 ; do
+  touch output_json/{email_data,variable_data}-${i}.json
+done
