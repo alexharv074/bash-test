c201872848b5c19de2899e53a1fc213d93d3940e
Alex Harvey <alexharv074@gmail.com>
2019-04-28 21:41:14 +1000

    Forgotten

--- test.sh
+++ test.sh
@@ -1,26 +1,7 @@
 cat > FILE <<EOF
-word1 word2 word3 word4
-word4 word5 word6 word7
-word6 word7 word8 word9
-word9 word6 word8 word3
+ 1556113878,60662402644292
+ 1554090396,59547403093308
 EOF
 
-awk '
-# script.awk
-
-NR > 1 {
-  split(last, last_ar)
-  split($0, curr_ar)
-
-  delete found          # Count how many unique occurrences
-  for (i in curr_ar)    # of words are seen.
-    for (j in last_ar)
-      if (last_ar[j] == curr_ar[i])
-        found[curr_ar[i]]++
-
-  if (length(found) >= 3) print
-}
-{
-  last = $0
-}
-' < FILE
+ruby -F, -ane '
+  print [Time.at($F[0].to_i).strftime("%Y-%m"), $F[1]].join(",")' FILE
