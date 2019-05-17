b6468f6f5b708f5653f691993aa1b971f0feb899
Alex Harvey <alexharv074@gmail.com>
2019-05-17 17:57:46 +1000

    Testing Todd's vim oneliner

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 cat > FILE <<EOF
-=======
-other
-text
-===================================
-v2.0.0
-
-Added feature 3
-Added feature 4
-===================================
-v1.0.0
-
-Added feature 1
-Added feature 2
-===================================
+the quick brown
+ fox jumped
+ "over
+   the
+... lazy dog
 EOF
 
-gsed -E '0,/^={35}$/d; //Q' FILE
+echo "input"
+cat FILE
+
+printf "\ntest 1\n"
+gsed -E 's/\b(.)/\u\1/' FILE
+
+printf "\ntest 2\n"
+cp FILE FILE1
+vim -c 'exe "%normal 0gUwl$" | wq!' FILE1 ; cat FILE1
+
+printf "\ntest 3\n"
+vim -c 'execute "%s/\\<\\(.\\)/\\u\\1/" | wq!' FILE ; cat FILE
